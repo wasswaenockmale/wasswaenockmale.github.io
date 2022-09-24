101021a4c79099b89a0c129b7861a7bf68282434
Wasswa Enock Male <enockmale97@gmail.com>
2022-09-24 17:48:27 +0300

    completed the logic of the typetext function in index.js file

--- js/index.js
+++ js/index.js
@@ -5,8 +5,26 @@ function typeText(str){
     const div = document.getElementById("header-left");
 
     // now, create a p tag.
-    const p = document.createElement('p');
+    const p = document.createElement('p')
 
+    // I have added the p tag to the div header-left.
+    div.appendChild(p)
+    let letter_count = 0;
+    const len = word.length;
+    setInterval(()=>{
+        const reg = /[.,]/
+        if(letter_count === len){
+            clearInterval()
+        }else{
+            if(reg.test(str[letter_count])){
+                p.innerHTML += str[letter_count] + "<br>"
+                letter_count++;
+            }else{
+                p.innerHTML += str[letter_count]
+                letter_count++;
+            }
+        }
+    },150)
     // Also, create a button tag
     const btn = document.createElement('button');
     btn.textContent = "HIRE ME"
@@ -14,9 +32,8 @@ function typeText(str){
     // also, create an hr tag
     const hr = document.createElement('hr')
     // now try to put the content to the p tag.
-    p.innerHTML = str
 
-    div.appendChild(p)
+    // p.innerHTML = str
     div.appendChild(btn)
     div.appendChild(hr);
 }
