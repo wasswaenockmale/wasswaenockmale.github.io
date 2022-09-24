ab5aefda0e9fabc5ad2a53e2bad4d15d70275e1b
Wasswa Enock Male <enockmale97@gmail.com>
2022-09-24 22:51:20 +0300

    I have completed the design of the button in the setInterval

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ const word = "Hi, I'm Wasswa Enock Male. A software developer. MERN Stack."
 function typeText(str){
     // Accept the string and them Start typing on the screen.
     const div = document.getElementById("header-left");
-
+    
     // now, create a p tag.
     const p = document.createElement('p')
 
@@ -11,15 +11,19 @@ function typeText(str){
     div.appendChild(p)
     let letter_count = 0;
     const len = word.length;
-    const btn = document.createElement('button');
-    div.appendChild(btn)
-    btn.focus()
+
+    const btn = document.createElement("button");
+    btn.textContent = "HIRE ME";
+    div.append(btn)
+    btn.classList.add("hide")
+    // setTimeout(()=>{
+
+    // },(100*len))
     setInterval(()=>{
         const reg = /[.,]/
         if(letter_count === len){
-            btn.textContent = "HIRE ME"
+            btn.classList.remove("hide");
             clearInterval()
-            // Also, create a button tag
         }else{
             if(reg.test(str[letter_count])){
                 p.innerHTML += str[letter_count] + "<br>"
@@ -31,9 +35,7 @@ function typeText(str){
         }
     },100)
     // also, create an hr tag
-    const hr = document.createElement('hr')
-    // now try to put the content to the p tag.
-    // p.innerHTML = str
+    const hr = document.createElement('hr');
     div.appendChild(hr);
 }
 
