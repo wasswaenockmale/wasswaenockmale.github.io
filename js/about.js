0e9a8310c1eb05748a9f5103a9a748a68226c956
Wasswa Enock Male <enockmale97@gmail.com>
2022-10-06 10:20:34 +0300

    removed the rest of the code for js

--- js/about.js
+++ js/about.js
@@ -4,32 +4,11 @@ console.log(archor)
 let arr_div = {'education':'none','skills':'none','experience':'none'}
 function select(){
     for(let k of archor){
+        if(k.hasAttribute('id')){
+            k.style.color = "blue"
+        }
         k.addEventListener('click',()=>{
-            const [i, _ ] =  k.attributes
-            const id = i.value.slice(1)
-            const div = document.getElementById(id)
-            div.setAttribute("display","block")
-            if(getKey('block') !== undefined){
-                /**
-                 * First remove element that has block first.
-                 * Then, add a new element that has blocked.
-                 */
-                const elem = getKey('block')
-                arr_div[elem] = "none"
-                document.getElementById(elem).setAttribute('display','none')
-                console.log("It is here")
-
-                // Let's work it out.
-                div.setAttribute('display','block')
-                div.style.color = "red"
-
-            }else{
-                console.log("It doesn't work here")
-                arr_div[id] = "block"
-                div.setAttribute('display','block')
-                div.style.color = "red"
-            }
-
+            
         });
     }
 }
