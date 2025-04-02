991bde3b0368aa9e83906368e3c9739b3a5a196f
wasswaenockmale <enockmale97@gmail.com>
2025-04-02 13:12:26 +0300

    fix: hot fix

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -3,21 +3,3 @@ export default {
     "@tailwindcss/postcss": {},
   }
 }
-
-// /** @type {import('tailwindcss').Config} */
-// export default {
-//   content: [
-//     "./src/**/*.{js,jsx,ts,tsx}",
-//   ],
-//   theme: {
-//     extend: {
-//       fontFamily: {
-//         poppins: ['Poppins', 'sans-serif'],
-//         inter: ['Inter', 'sans-serif'],
-//         tangerine: ["Tangerine", "cursive"],
-//       },
-//     },
-//   },
-//   plugins: [],
-// }
-
